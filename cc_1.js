a71d400def892197ce727c84e0e679d604443ef5
Jordon Venner <jordonvenner@usf.edu>
2025-01-24 14:24:30 -0500

    Task 1 - Employee Information.

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,16 @@
 //JavaScript Basics Challenge - Task 1
+
+let employeeName = 'Jordon'
+const employeeID = 123456
+var isActive = true
+
+console.log(employeeName)
+console.log(typeof(employeeName))
+
+console.log(employeeID)
+console.log(typeof(employeeID))
+
+console.log(isActive)
+console.log(typeof(isActive))
+
+
