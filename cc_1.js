33689afac2a17145a3fca9be7032b1309f32f417
Jordon Venner <jordonvenner@usf.edu>
2025-01-24 14:56:37 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -1,16 +1,34 @@
 //JavaScript Basics Challenge - Task 1
 
-let employeeName = 'Jordon'
-const employeeID = 123456
-var isActive = true
 
-console.log(employeeName)
-console.log(typeof(employeeName))
+let employeeName = 'Jordon';
+const employeeID = 123456;
+var isActive = true;
 
-console.log(employeeID)
-console.log(typeof(employeeID))
+console.log(employeeName);
+console.log(typeof(employeeName));
 
-console.log(isActive)
-console.log(typeof(isActive))
+console.log(employeeID);
+console.log(typeof(employeeID));
+
+console.log(isActive);
+console.log(typeof(isActive));
+
+//JavaScript Basics Challenge - Task 2
+
+
+let productName = 'Stapler';
+const productPrice = '16';
+var isAvailable = 'true';
+
+console.log(productName);
+console.log(typeof(stapler));
+
+console.log(16);
+console.log(typeof(16));
+
+console.log(isActive);
+console.log(typeof(isActive));
+
 
 
